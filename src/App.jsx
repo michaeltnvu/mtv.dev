5ee345fe74ce3a8985d37cc02fb965fbf41c9f24
Michael Vu <vu.michaeltien@gmail.com>
2024-01-26 21:17:53 -0500

    added daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <div>Hi.</div>;
+  return (
+    <div className="flex items-center justify-center h-[100vh] w-[100vw]">
+      <div className="mockup-browser border bg-base-300 w-[80vw] h-[60vh]">
+        <div className="mockup-browser-toolbar">
+          <div className="input">Hi, my name is Michael Vu</div>
+        </div>
+        <div className="flex justify-center px-4 py-16 bg-base-200">
+          I am an ex-collegiate athlete, an ex-accountant, and ex-project
+          manager turned Full-stack Developer.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
