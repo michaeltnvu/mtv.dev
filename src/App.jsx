29762c4cf4ee5bd809fd29ec35d9a112f117ef0f
Michael Vu <vu.michaeltien@gmail.com>
2024-01-27 23:55:35 -0500

    removed browser mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-[100vh] w-[100vw]">
-      <div className="mockup-browser border bg-base-300 w-[80vw] h-[60vh]">
-        <div className="mockup-browser-toolbar">
-          <div className="input">Hi, my name is Michael Vu</div>
-        </div>
-        <div className="flex justify-center px-4 py-16 bg-base-200">
-          I am an ex-collegiate athlete, an ex-accountant, and ex-project
-          manager turned Full-stack Developer.
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
+      <h1 className="text-3xl">
+        I'm <span className="font-bold">Michael Vu</span>.
+      </h1>
+      <p className="text-xl">
+        An ex-collegiate athlete, an ex-accountant, and ex-project manager
+        turned <span className="font-bold">Full-stack Developer.</span>
+      </p>
     </div>
   );
 };
