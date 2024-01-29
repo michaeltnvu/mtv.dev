976380a54617aead882b23ee9a75b3f378fc7974
Michael Vu <vu.michaeltien@gmail.com>
2024-01-28 22:52:03 -0500

    outlining portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
-      <h1 className="text-3xl">
-        I'm <span className="font-bold">Michael Vu</span>.
-      </h1>
-      <p className="text-xl">
-        An ex-collegiate athlete, an ex-accountant, and ex-project manager
-        turned <span className="font-bold">Full-stack Developer.</span>
-      </p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[100vh] w-[100vw]">
+      <h1 className="text-3xl">
+        Former accountant and project manager turned Full Stack Developer.
+        Welcome to my online portfolio.
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
