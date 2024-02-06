f2b52ad9e43095aded6f26bf6da019ed219581a2
Michael Vu <vu.michaeltien@gmail.com>
2024-02-05 20:28:40 -0500

    listed projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,11 @@ const Projects = () => {
   return (
     <div className="px-[5%] py-[15%]">
       <h2 className="font-bold text-3xl">Projects</h2>
-      <div></div>
+      <ul>
+        <li>Pathfindr</li>
+        <li>Task Trove</li>
+        <li>Ironhack Samurai</li>
+      </ul>
     </div>
   );
 };
