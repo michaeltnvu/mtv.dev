d1da79c4cd3fa586f760e7c6c3f674329ce25d12
Michael Vu <vu.michaeltien@gmail.com>
2024-05-01 19:48:18 -0400

    connected emailjs service to contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,98 @@
-const Contact = () => {
-  return <></>;
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+interface ContactFormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const Contact: React.FC = () => {
+  const [statusMessage, setStatusMessage] = useState("");
+  const [formData, setFormData] = useState<ContactFormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID || "";
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID || "";
+
+
+    try {
+      await emailjs.send(
+        serviceId, 
+        templateId,
+        {
+          from_name: formData.name,
+          from_email: formData.email,
+          message: formData.message,
+        },
+        publicKey
+      );
+
+      setStatusMessage("Email sent successfully!");
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setStatusMessage("Failed to send email. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Let's connect!</h2>
+      {statusMessage && <p>{statusMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea 
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
